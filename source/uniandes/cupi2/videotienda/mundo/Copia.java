3d861752c506e6e176cb8f7b4741d859b42a28b1
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-06-10 12:27:26 -0500

    clase copia

--- source/uniandes/cupi2/videotienda/mundo/Copia.java
+++ source/uniandes/cupi2/videotienda/mundo/Copia.java
@@ -1,24 +1,53 @@
 package uniandes.cupi2.videotienda.mundo;
 
+/**
+ * This class represents a copy of a movie in the video store
+ */
 public class Copia {
-	
+    
+    /**
+     * Movie title this copy belongs to
+     */
     private String tituloPelicula;
     
+    /**
+     * Unique identifier code for this copy
+     */
     private int codigo;
     
-    public Copia( String laPelicula, int elCodigo) {
-    	
+    /**
+     * Creates a new copy of a movie
+     * @param laPelicula Movie title. laPelicula != null
+     * @param elCodigo Unique identifier code. elCodigo > 0
+     */
+    public Copia(String laPelicula, int elCodigo) {
+        tituloPelicula = laPelicula;
+        codigo = elCodigo;
     }
     
+    /**
+     * Returns the copy's code
+     * @return copy identifier code
+     */
     public int darCodigo() {
-    	
+        return codigo;
     }
     
+    /**
+     * Returns the movie title
+     * @return movie title
+     */
     public String darTituloPelicula() {
-    	
+        return tituloPelicula;
     }
 
-    public boolean esIgualA( Copia otra ){
-
+    /**
+     * Checks if this copy is equal to another one
+     * @param otra Other copy to compare with. otra != null
+     * @return true if both copies have the same movie title and code, false otherwise
+     */
+    public boolean esIgualA(Copia otra) {
+        return tituloPelicula.equals(otra.darTituloPelicula()) && 
+               codigo == otra.darCodigo();
     }
 }
