94700e29e84c61b4f7b35e47cf46ea31a71c529e
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-06-10 09:13:31 -0500

    Clase Copia agregada

---
+++ source/uniandes/cupi2/videotienda/mundo/Copia.java
@@ -0,0 +1,24 @@
+package uniandes.cupi2.videotienda.mundo;
+
+public class Copia {
+	
+    private String tituloPelicula;
+    
+    private int codigo;
+    
+    public Copia( String laPelicula, int elCodigo) {
+    	
+    }
+    
+    public int darCodigo() {
+    	
+    }
+    
+    public String darTituloPelicula() {
+    	
+    }
+
+    public boolean esIgualA( Copia otra ){
+
+    }
+}
